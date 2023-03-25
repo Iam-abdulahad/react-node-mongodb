0c75005c301fde6132514e24bc4182364608e173
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-03-25 14:50:54 +0600

    first commit

---
+++ src/Components/AddUser/AddUser.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddUser = () => {
+          return (
+                    <div>
+                              Please add a users
+                    </div>
+          );
+};
+
+export default AddUser;
