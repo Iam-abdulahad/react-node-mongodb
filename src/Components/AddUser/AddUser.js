44c47cda0142ee34bf559b9495beed37d4922b62
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-03-25 15:06:15 +0600

    added user form

--- src/Components/AddUser/AddUser.js
+++ src/Components/AddUser/AddUser.js
@@ -3,7 +3,14 @@ import React from 'react';
 const AddUser = () => {
           return (
                     <div>
-                              Please add a users
+                              <h1>Please Register as a new user</h1>
+                              <form action="">
+                                        <input type="text" name='name' placeholder='Name' required />
+                                        <br />
+                                        <input type="email" name='email' placeholder='Email' required />
+                                        <br />
+                                        <input type="submit" value="submit" />
+                              </form>
                     </div>
           );
 };
