b9da0699b1944e7fbfa8c761b356ce3c919d0b15
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-03-27 10:06:27 +0600

    fixed the error

--- src/Components/AddUser/AddUser.js
+++ src/Components/AddUser/AddUser.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
 const AddUser = () => {
+          const handleAddUser = event => {
+                    event.preventDefault();
+                    const name = event.target.name.value;
+                    const email = event.target.email.value;
+
+                    const user = { name, email };
+
+                    fetch('', {
+                              method: 'POST',
+                              headers: {
+                                        'content-type': 'application/json'
+                              },
+                              body: JSON.stringify(user)
+                    })
+
+                    .then(res => res.json())
+                    .then(data => console.log("success:", data))
+          }
           return (
                     <div>
                               <h1>Please Register as a new user</h1>
-                              <form action="">
+                              <form onSubmit={handleAddUser}>
                                         <input type="text" name='name' placeholder='Name' required />
                                         <br />
                                         <input type="email" name='email' placeholder='Email' required />
