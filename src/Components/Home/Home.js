27c5eda80c5ac115973d4480b19ab736621df5aa
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-03-31 09:15:46 +0600

    delete user from database

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,14 +6,26 @@ const Home = () => {
                     fetch('http://localhost:5000/user')
                     .then(res => res.json())
                     .then(data => setUsers(data));
-          } ,[])
+          } ,[]);
+
+          const handleUserDelete = id =>{
+                    const proceed = window.confirm('you wanna delete this user?')
+
+                    if(proceed){
+                              console.log('This user will be delete', id);
+                    }
+
+          }
           return (
                     <div>
                               <h2>Available User: {users.length}</h2>
                               <ul>
                                         {
-                                                  users.map(user => <li>
+                                                  users.map(user => <li
+                                                  key={user._id}
+                                                  >
                                                             {user.name}: {user.email}
+                                                            <button onClick={() => handleUserDelete(user._id)}>X</button>
                                                   </li>)
                                         }
                               </ul>
